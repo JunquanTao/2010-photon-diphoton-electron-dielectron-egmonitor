b6f7812abf7e86df6f0e7e5a9278bddc25f53af9
jtao <jtao@cern.ch>
2018-07-24 17:26:30 +0200

    New

---
+++ PhotonElectronAnalyzer/test/PhotonElectronAnalyzer.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring( 
+    "file:./DYphotospp_Tune4C_13TeV_pythia8_cfi_py_GEN_VALIDATION.root"
+#     "file:./DYnorad_Tune4C_13TeV_pythia8_cfi_py_GEN_VALIDATION.root"
+    )
+)
+
+process.demo = cms.EDAnalyzer('PhotonElectronAnalyzer',
+ outputfileName = cms.untracked.string("Gen_DY.root"),
+ HepMCLabel = cms.untracked.InputTag('generatorSmeared','')
+)
+
+process.out_step = cms.EndPath(process.demo)
